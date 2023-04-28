7143f879df6042e721d146441b330ead6df8def7
Sema Dilbaz Boncuk <shboncuk@gmail.com>
2023-04-28 13:29:54 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -2,5 +2,7 @@ public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir.");
         //version1
+        System.out.println("java is easy");
+        //version2
     }
 }
